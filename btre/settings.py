78c98b2e511cedce7993fda521b181dba3c1a78c
WenxuC <wenx.chen@outlook.com>
2022-04-07 15:05:30 -0400

    contact form submission

--- btre/settings.py
+++ btre/settings.py
@@ -145,3 +145,9 @@ MESSAGE_TAGS = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email Config
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER='--email placeholder--'
+# EMAIL_HOST_PASSWORD=''
+# EMAIL_USE_TLS=True
